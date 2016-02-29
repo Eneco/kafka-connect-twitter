05ab71f067045b8ed6f0aedadad9cb59c80cd8bd
Rollulus <roelboel@xs4all.nl>
2016-02-29 19:44:14 +0100

    Serialise a more complete Status

--- src/main/scala/com/eneco/trading/kafka/connect/twitter/TwitterStreamReader.scala
+++ src/main/scala/com/eneco/trading/kafka/connect/twitter/TwitterStreamReader.scala
@@ -1,19 +1,14 @@
 package com.eneco.trading.kafka.connect.twitter
 
 import java.util.Collections
-import java.util.concurrent.{Executors, TimeUnit, LinkedBlockingQueue}
-
-import com.sun.xml.internal.ws.api.message.Packet
+import java.util.concurrent.{Executors, LinkedBlockingQueue, TimeUnit}
 import com.twitter.hbc.httpclient.BasicClient
 import com.twitter.hbc.twitter4j.Twitter4jStatusClient
-import org.apache.kafka.connect.data.Schema;
-import org.apache.kafka.connect.data.SchemaBuilder;
-import org.apache.kafka.connect.data.Struct;
-import org.apache.kafka.connect.source.SourceRecord;
-import org.apache.kafka.connect.data.Struct
+import org.apache.kafka.connect.data.{Schema, SchemaBuilder, Struct}
 import org.apache.kafka.connect.source.SourceRecord
-import twitter4j.{StallWarning, Status, StatusDeletionNotice, StatusListener}
+import twitter4j._
 import scala.collection.JavaConverters._
+
 
 class StatusEnqueuer(queue: LinkedBlockingQueue[Status]) extends StatusListener with Logging {
   override def onStallWarning(stallWarning: StallWarning): Unit = {
@@ -42,6 +37,51 @@ class StatusEnqueuer(queue: LinkedBlockingQueue[Status]) extends StatusListener 
   }
 }
 
+case class TwitterStatus(id: Long, createdAt: String, favoriteCount: Int, text: String, user: TwitterUser)
+case class TwitterUser(id: Long, name: String, screenName: String)
+
+object TwitterUser {
+  def apply(u: User) = {
+    new TwitterUser(id = u.getId, name = u.getName, screenName = u.getScreenName)
+  }
+
+  def struct(u: TwitterUser) =
+    new Struct(schema)
+      .put("id", u.id)
+      .put("name", u.name)
+      .put("screenName", u.screenName)
+
+  val schema = SchemaBuilder.struct().name("TwitterUser")
+    .field("id", Schema.INT64_SCHEMA)
+    .field("name", Schema.STRING_SCHEMA)
+    .field("screenName", Schema.STRING_SCHEMA)
+    .build()
+}
+
+object TwitterStatus {
+  def apply(s: Status) = {
+    new TwitterStatus(id = s.getId, createdAt = s.getCreatedAt.toString, favoriteCount = s.getFavoriteCount, text = s.getText, user = TwitterUser(s.getUser))
+  }
+
+  def struct(s: TwitterStatus) =
+    new Struct(schema)
+      .put("id", s.id)
+      .put("createdAt", s.createdAt)
+      .put("favoriteCount", s.favoriteCount)
+      .put("text", s.text)
+      .put("user", TwitterUser.struct(s.user))
+
+  val schema = SchemaBuilder.struct().name("TwitterStatus")
+    .field("id", Schema.INT64_SCHEMA)
+    .field("createdAt", Schema.STRING_SCHEMA)
+    .field("favoriteCount", Schema.INT32_SCHEMA)
+    .field("text", Schema.STRING_SCHEMA)
+    .field("user", TwitterUser.schema)
+    .build()
+}
+
+
+
 /**
   * Created by andrew@datamountaineer.com on 24/02/16. 
   * kafka-connect-twitter
@@ -62,12 +102,8 @@ class TwitterStreamReader(client: BasicClient, rawqueue: LinkedBlockingQueue[Str
     }
     return Option(statusqueue.poll(1, TimeUnit.SECONDS)) match {
       case Some(status) => {
-        val sch = SchemaBuilder.struct().name("status")
-            .field("text",Schema.STRING_SCHEMA)
-          .field("user",Schema.STRING_SCHEMA)
-          .build()
-        val s = new Struct(sch).put("text",status.getText).put("user",status.getUser.getScreenName)
-        List[SourceRecord](new SourceRecord(Collections.singletonMap("TODO", "TODO"), Collections.singletonMap("TODO2", "TODO2"), "test", sch, s))
+        val ts = TwitterStatus.struct(TwitterStatus(status))
+        List[SourceRecord](new SourceRecord(Collections.singletonMap("TODO", "TODO"), Collections.singletonMap("TODO2", "TODO2"), "tweeters", ts.schema(), ts))
       }
       case _ => List[SourceRecord]()
     }
