cc4a6e7c0f869d1a79626533cbaf6adce8504dcb
Rollulus <roelboel@xs4all.nl>
2016-03-01 16:22:24 +0100

    Put timeout on batch; fix #7

--- src/main/scala/com/eneco/trading/kafka/connect/twitter/TwitterStatusReader.scala
+++ src/main/scala/com/eneco/trading/kafka/connect/twitter/TwitterStatusReader.scala
@@ -1,14 +1,14 @@
 package com.eneco.trading.kafka.connect.twitter
 
 import java.util
-import java.util.concurrent.{Executors, LinkedBlockingQueue}
+import java.util.concurrent.{TimeUnit, LinkedBlockingQueue, Executors}
 import com.eneco.trading.kafka.connect.twitter.domain.TwitterStatus
 import com.twitter.hbc.httpclient.BasicClient
 import com.twitter.hbc.twitter4j.Twitter4jStatusClient
 import org.apache.kafka.connect.source.SourceRecord
 import twitter4j._
 import scala.collection.JavaConverters._
-
+import Extensions._
 
 class StatusEnqueuer(queue: LinkedBlockingQueue[Status]) extends StatusListener with Logging {
   override def onStallWarning(stallWarning: StallWarning) = log.warn("onStallWarning")
@@ -52,8 +52,11 @@ class TwitterStatusReader(client: BasicClient, rawQueue: LinkedBlockingQueue[Str
     * @return A List of SourceRecords
     * */
   def poll() : util.List[SourceRecord] = {
-    if (client.isDone) log.warn("Client connection closed unexpectedly: ", client.getExitEvent.getMessage)
-    (1 to batchSize).map(i=>statusQueue.take()).map(r=>buildRecords(r)).toList.asJava
+    if (client.isDone) log.warn("Client connection closed unexpectedly: ", client.getExitEvent.getMessage) //TODO: what next?
+
+    val l = new util.ArrayList[Status]()
+    statusQueue.drainWithTimeoutTo(l, batchSize, 1, TimeUnit.SECONDS)
+    l.asScala.map(buildRecords).asJava
   }
 
   /**
