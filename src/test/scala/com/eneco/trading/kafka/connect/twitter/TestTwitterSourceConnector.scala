b435b5eb2119df371bb09c822f82ecd452973f2d
Rollulus <roelboel@xs4all.nl>
2016-03-01 11:29:16 +0100

    Test TestTwitterSourceConnector.start

--- src/test/scala/com/eneco/trading/kafka/connect/twitter/TestTwitterSourceConnector.scala
+++ src/test/scala/com/eneco/trading/kafka/connect/twitter/TestTwitterSourceConnector.scala
@@ -1,9 +1,24 @@
 package com.eneco.trading.kafka.connect.twitter
 
+import scala.collection.JavaConverters._
+
 /**
   * Created by andrew@datamountaineer.com on 29/02/16. 
   * kafka-connect-twitter
   */
-class TestTwitterSourceConnector extends  TestTwitterBase {
+class TestTwitterSourceConnector extends TestTwitterBase {
+  val goodProps = getConfig
+  val badProps = goodProps + (TwitterSourceConfig.BATCH_SIZE -> "this is no integer")
 
+  test("A TwitterSourceConnector should start with valid properties") {
+    val t = new TwitterSourceConnector()
+    t.start(goodProps.asJava)
+  }
+
+  test("A TwitterSourceConnector shouldn't start with invalid properties") {
+    val t = new TwitterSourceConnector()
+    an[Exception] should be thrownBy {
+      t.start(badProps.asJava)
+    }
+  }
 }
