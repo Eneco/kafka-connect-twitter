9f167d1d2be8aab6ba9e70111ea994e4733065fb
Rollulus <roelboel@xs4all.nl>
2016-04-28 16:27:59 +0200

    Document SimpleTwitterWriter

--- src/main/scala/com/eneco/trading/kafka/connect/twitter/TwitterWriter.scala
+++ src/main/scala/com/eneco/trading/kafka/connect/twitter/TwitterWriter.scala
@@ -6,13 +6,31 @@ import twitter4j.{TwitterFactory, Twitter}
 import scala.util.Try
 
 trait SimpleTwitterWriter {
+  /**
+    * Performs a status update
+    * @param s the status
+    * @return A Try id of the new status as assigned by the Twitter api
+    */
   def updateStatus(s: String): Try[Long]
 }
 
+/**
+  * Allows one to update statuses
+  * @param consumer apps.twitter.com | Keys and Access Tokens: 	Consumer Key (API Key)
+  * @param consumerSecret apps.twitter.com | Keys and Access Tokens: Consumer Secret (API Secret)
+  * @param access apps.twitter.com | Keys and Access Tokens: Access Token
+  * @param accessSecret apps.twitter.com | Keys and Access Tokens: Access Token Secret
+  * @param twitterClient poor man's DI: something that implements the Twitter4j Twitter interface
+  */
 class TwitterWriter(consumer: String, consumerSecret: String, access: String, accessSecret: String, twitterClient: Twitter = new TwitterFactory().getInstance()) extends SimpleTwitterWriter {
   twitterClient.setOAuthConsumer(consumer, consumerSecret)
   twitterClient.setOAuthAccessToken(new AccessToken(access, accessSecret))
 
+  /**
+    * Performs a status update
+    * @param s the status
+    * @return A Try id of the new status as assigned by the Twitter api
+    */
   def updateStatus(s: String): Try[Long] = {
     Try(twitterClient.updateStatus(s).getId)
   }
