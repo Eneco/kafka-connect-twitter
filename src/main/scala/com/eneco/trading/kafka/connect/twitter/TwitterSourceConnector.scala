1f718b5b57cc0c17c389996a95a353dbb0086d85
Jakub Scholz <www@scholzj.com>
2018-05-11 16:53:48 +0200

    TwitterSourceConnector should extend SourceConnector and not just Connector

--- src/main/scala/com/eneco/trading/kafka/connect/twitter/TwitterSourceConnector.scala
+++ src/main/scala/com/eneco/trading/kafka/connect/twitter/TwitterSourceConnector.scala
@@ -1,8 +1,9 @@
 package com.eneco.trading.kafka.connect.twitter
 
 import java.util
-import org.apache.kafka.connect.connector.{Task, Connector}
+import org.apache.kafka.connect.connector.Task
 import org.apache.kafka.connect.errors.ConnectException
+import org.apache.kafka.connect.source.SourceConnector
 import scala.collection.JavaConverters._
 import scala.util.{Failure, Try}
 
@@ -10,7 +11,7 @@ import scala.util.{Failure, Try}
   * Created by andrew@datamountaineer.com on 24/02/16.
   * kafka-connect-twitter
   */
-class TwitterSourceConnector extends Connector with Logging {
+class TwitterSourceConnector extends SourceConnector with Logging {
   private var configProps : util.Map[String, String] = null
 
   /**
